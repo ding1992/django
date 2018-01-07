e558457b542cf8714d7b66f82fc8f00a67c4d725
guorui <guorui.ding@gmail.com>
2018-01-07 15:18:53 +0800

    Github Starred Repo

--- GithubStarredRepo/settings.py
+++ GithubStarredRepo/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cba2d8bd-e8e3-4fe5-8820-6315f9c90f99'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.230.68.153',
+                 '127.0.0.1']
 
 
 # Application definition
